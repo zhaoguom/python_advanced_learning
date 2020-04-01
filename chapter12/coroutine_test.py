d875bb5889a6bf254226cbc5f1994e8e6b4a5d3b
zhaoguo.ma <zhaoguo.ma@arcserve.com>
2020-04-01 12:45:22 +0800

    test

--- chapter12/coroutine_test.py
+++ chapter12/coroutine_test.py
@@ -11,3 +11,19 @@
     - 不再需要锁，并发性高，单线程内的函数切换性能远高于线程切换，并发性更高
 - 协程：可以暂停的函数，并且可以从暂停的地方传入值
 '''
+
+def gen_func():
+    #1. 可以产出值 2. 可以接收值
+    html = yield "http://www.baidu.com"
+    print(html)
+    yield 2
+    yield 3
+    return "zhaoguom"
+
+if __name__ == "__main__":
+    gen = gen_func()
+    url = next(gen)
+    html = "zhaoguom"
+    # 启动生成器的方式有2中，next(), send()
+    # send()方法可以传递值进入生成器内部，同时还可以重启生成器执行到下一个yield
+    print(gen.send(html))
